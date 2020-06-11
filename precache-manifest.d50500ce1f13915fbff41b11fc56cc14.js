f3a5821e514236b54c6dc05e7ec2e51f8f074018
Sergio <sergio.rod.perez@outlook.es>
2020-06-10 21:25:15 -0500

    Updates

---
+++ precache-manifest.d50500ce1f13915fbff41b11fc56cc14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3163a76f666469be20d7d8b656f781",
+    "url": "/IglesiaCDIBlank/index.html"
+  },
+  {
+    "revision": "008fa7d66137c05f3acd",
+    "url": "/IglesiaCDIBlank/static/css/main.f5614e4b.chunk.css"
+  },
+  {
+    "revision": "ce5205ec72052476a323",
+    "url": "/IglesiaCDIBlank/static/js/2.a8b897f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IglesiaCDIBlank/static/js/2.a8b897f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "008fa7d66137c05f3acd",
+    "url": "/IglesiaCDIBlank/static/js/main.91feacb3.chunk.js"
+  },
+  {
+    "revision": "a24c3b07a369d1974690",
+    "url": "/IglesiaCDIBlank/static/js/runtime-main.f3672690.js"
+  },
+  {
+    "revision": "1091b4c7d3fe7c520494416fe6dfe1d8",
+    "url": "/IglesiaCDIBlank/static/media/thumbnail_transmision.1091b4c7.png"
+  }
+]);
