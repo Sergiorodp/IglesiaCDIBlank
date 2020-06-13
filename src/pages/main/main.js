7919155190e5ef87f287459f09232af0af66427c
Sergio <sergio.rod.perez@outlook.es>
2020-06-12 21:43:29 -0500

    nuevo diseño servicio online

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Welcome from './images/Bienvenidos@72x.png'
+// import Welcome from './images/Bienvenidos@72x.png'
 import './main.css'
 
 // Componentes
@@ -11,9 +11,9 @@ function MainPage(){
         <div className = "body-main-page">
             {/* <h1 className = " InProgress ">En proceso...</h1> */}
             <div className = "welcome-grid-container">
-                <div className="Welcome-img-container">
+                {/* <div className="Welcome-img-container">
                     <img src = {Welcome} alt = "welcome" className = "Welcome-image"></img>
-                </div>
+                </div> */}
             </div>
             <Intro/>
             <div className = "container-grid">
