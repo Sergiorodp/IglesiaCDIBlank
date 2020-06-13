1ab49c4f14976ed97ed10b1d1a5c18d11eb4436a
Sergio <sergio.rod.perez@outlook.es>
2020-06-12 20:44:19 -0500

    agregar imagen bienvenidos

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Welcome from './images/Bienvenidos@72x.png'
+import './main.css'
 
 // Componentes
 import Intro from '../../Components/plantilla-intro/Intro'
@@ -7,9 +9,15 @@ import Intro from '../../Components/plantilla-intro/Intro'
 function MainPage(){
     return(
         <div className = "body-main-page">
-            <h1>En proceso...</h1>
+            {/* <h1 className = " InProgress ">En proceso...</h1> */}
+            <div className = "welcome-grid-container">
+                <div className="Welcome-img-container">
+                    <img src = {Welcome} alt = "welcome" className = "Welcome-image"></img>
+                </div>
+            </div>
             <Intro/>
             <div className = "container-grid">
+                {/* <div className = "icons-container"></div> */}
             </div>
         </div>
     )
